98001d753681323f54599d47716099c6a43fa52f
wyuchen1217 <68698324+wyuchen1217@users.noreply.github.com>
2023-07-17 13:53:50 -0400

    challenge 1 finished

--- 1. Getting Started/Challenge 1/YourInitials.java
+++ 1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,16 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("Y   Y  W   W");
+         System.out.println("Y   Y  W   W");
+         System.out.println(" Y Y   W   W");
+         System.out.println("  Y    W W W");
+         System.out.println("  Y    WW WW");
+         System.out.println("  Y    WW WW");
+         System.out.println("  Y    W   W");
+
+
+
+
     }
 }
